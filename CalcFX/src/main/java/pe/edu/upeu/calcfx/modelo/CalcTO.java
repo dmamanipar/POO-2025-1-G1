fb89ae21d006c0dca637531626eebf6e9f72c53e
dmamanipar <dmamanipar@unsa.edu.pe>
2025-05-02 12:05:07 -0500

    CRUD spring p1

--- CalcFX/src/main/java/pe/edu/upeu/calcfx/modelo/CalcTO.java
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/modelo/CalcTO.java
@@ -1,40 +1,17 @@
 package pe.edu.upeu.calcfx.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.context.annotation.Primary;
+
+@Entity(name = "calculadora")
+@Data
 public class CalcTO {
+    @Id
+    Long id;
     String num1;
     String num2;
     char operador;
     String resultado;
-
-    public String getNum1() {
-        return num1;
-    }
-
-    public void setNum1(String num1) {
-        this.num1 = num1;
-    }
-
-    public String getNum2() {
-        return num2;
-    }
-
-    public void setNum2(String num2) {
-        this.num2 = num2;
-    }
-
-    public char getOperador() {
-        return operador;
-    }
-
-    public void setOperador(char operador) {
-        this.operador = operador;
-    }
-
-    public String getResultado() {
-        return resultado;
-    }
-
-    public void setResultado(String resultado) {
-        this.resultado = resultado;
-    }
 }
