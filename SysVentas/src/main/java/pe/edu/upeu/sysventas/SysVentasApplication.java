0d0d648d0c8cdedd0d6092e970ba03ec23e90cf0
dmamanipar <dmamanipar@unsa.edu.pe>
2025-05-13 15:58:18 -0500

    Servicio parte 1

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
@@ -1,13 +1,31 @@
 package pe.edu.upeu.sysventas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pe.edu.upeu.sysventas.modelo.Marca;
+import pe.edu.upeu.sysventas.repositorio.EmisorRepository;
+import pe.edu.upeu.sysventas.repositorio.MarcaRepository;
+
+import java.util.List;
 
 @SpringBootApplication
-public class SysVentasApplication {
+public class SysVentasApplication /*implements CommandLineRunner*/ {
+
+	@Autowired
+	private MarcaRepository marcaRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SysVentasApplication.class, args);
 	}
 
+	/*@Override
+	public void run(String... args) throws Exception {
+		List<Marca> listaMarcas = marcaRepository.findAll();
+		for (Marca marca : listaMarcas) {
+			System.out.println(marca.getNombre());
+		}
+	}*/
 }
